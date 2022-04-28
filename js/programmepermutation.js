935c68d9ce3a5a2e9d72ff0f4b45776e6c8098ba
Rachid <rachid.rhazid@lepoles.org>
2022-04-28 17:03:55 +0200

    28.04

---
+++ js/programmepermutation.js
@@ -0,0 +1,13 @@
+var nombre1 = 5;
+var nombre2 = 3;
+
+var nombre3;
+
+
+nombre3 = nombre1;
+nombre1 = nombre2;
+nombre2 = nombre3;
+
+console.log("nombre1=",nombre1)
+console.log("nombre2=", nombre2)
+
