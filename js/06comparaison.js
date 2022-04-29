b22789de6b1693de5979d0eb7a83657aed59af4e
Rachid <rachid.rhazid@lepoles.org>
2022-04-29 16:50:28 +0200

    29.04

---
+++ js/06comparaison.js
@@ -0,0 +1,11 @@
+// var dollar = 1.0547;
+// var euro = prompt ("Valeur a Convertir");
+// var dollar = euro * dollar;
+// document.write(dollar);
+
+// prompt("message aleatoir");
+
+// var teste = document.write("blablabla");
+// console.log
+let test = prompt("message");
+console.log(typeof test);
