a812e7f1a0951b4634af3e633491629018b82a74
Rachid <rachid.rhazid@lepoles.org>
2022-05-02 17:00:50 +0200

    02.05

---
+++ js/exo_02_05_22.js
@@ -0,0 +1,29 @@
+// var monaieExercice = ("je suis en train de faire mon exercice");
+
+// document.write(monaieExercice);
+
+// console.log(monaieExercice);
+
+// alert(monaieExercice)
+
+// var monChiffre = 2022;
+// document.write(typeof monChiffre + "<br>");
+
+// var maDecimale = 3.14;
+// document.write(typeof maDecimale + "<br>");
+
+var maChainedeCharactere = "57.12";
+console.log(typeof maChainedeCharactere);
+document.write(maChainedeCharactere + "&nbsp");
+
+maChainedeCharactere = parseFloat (maChainedeCharactere);
+console.log(typeof maChainedeCharactere);
+document.write(maChainedeCharactere + "&nbsp");
+
+maChainedeCharactere = parseInt (maChainedeCharactere);
+console.log(typeof maChainedeCharactere);
+document.write(maChainedeCharactere + "&nbsp");
+
+maChainedeCharactere = maChainedeCharactere.toString();
+console.log(typeof maChainedeCharactere);
+document.write(maChainedeCharactere);
