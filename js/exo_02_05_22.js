fa629ad0e05a6402cdae556951495cb81034b761
Rachid <rachid.rhazid@lepoles.org>
2022-05-03 16:57:08 +0200

    03.05

--- js/exo_02_05_22.js
+++ js/exo_02_05_22.js
@@ -12,18 +12,48 @@
 // var maDecimale = 3.14;
 // document.write(typeof maDecimale + "<br>");
 
-var maChainedeCharactere = "57.12";
-console.log(typeof maChainedeCharactere);
-document.write(maChainedeCharactere + "&nbsp");
+// var maChainedeCharactere = "57.12";
+// console.log(typeof maChainedeCharactere);
+// document.write(maChainedeCharactere + "&nbsp");
 
-maChainedeCharactere = parseFloat (maChainedeCharactere);
-console.log(typeof maChainedeCharactere);
-document.write(maChainedeCharactere + "&nbsp");
+// maChainedeCharactere = parseFloat (maChainedeCharactere);
+// console.log(typeof maChainedeCharactere);
+// document.write(maChainedeCharactere + "&nbsp");
 
-maChainedeCharactere = parseInt (maChainedeCharactere);
-console.log(typeof maChainedeCharactere);
-document.write(maChainedeCharactere + "&nbsp");
+// maChainedeCharactere = parseInt (maChainedeCharactere);
+// console.log(typeof maChainedeCharactere);
+// document.write(maChainedeCharactere + "&nbsp");
 
-maChainedeCharactere = maChainedeCharactere.toString();
-console.log(typeof maChainedeCharactere);
-document.write(maChainedeCharactere);
+// maChainedeCharactere = maChainedeCharactere.toString();
+// console.log(typeof maChainedeCharactere);
+// document.write(maChainedeCharactere);
+
+var annee = 2017;
+var future = 3;
+var calcul = annee + future;
+console.log(calcul);
+document.write(calcul + "&nbsp &nbsp" + calcul + "<br>");
+
+var nb1, nb2, resultat;
+nb1 = 10;
+nb2 = 5;
+
+resultat = nb1 + nb2;
+console.log(resultat);
+document.write(resultat + "<br>");
+
+var resultatSoustraction = nb1 - nb2;
+console.log(resultatSoustraction);
+document.write(resultatSoustraction + "<br>");
+
+var resultatMultiplication = nb1 * nb2;
+console.log(resultatMultiplication);
+document.write(resultatMultiplication + "<br>");
+
+var resultatDivision = nb1 / nb2;
+console.log(resultatDivision);
+document.write(resultatDivision + "<br>");
+
+var resultatModulo= nb1 % nb2;
+console.log(resultatModulo);
+document.write(resultatModulo + "<br>");
