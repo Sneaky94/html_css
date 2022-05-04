d115a44b8122ac3a0aa7cf31e390b3603ec320da
Rachid <rachid.rhazid@lepoles.org>
2022-05-04 16:59:31 +0200

    04.05c1

--- js/exo_02_05_22.js
+++ js/exo_02_05_22.js
@@ -28,32 +28,40 @@
 // console.log(typeof maChainedeCharactere);
 // document.write(maChainedeCharactere);
 
-var annee = 2017;
-var future = 3;
-var calcul = annee + future;
-console.log(calcul);
-document.write(calcul + "&nbsp &nbsp" + calcul + "<br>");
+// var annee = 2017;
+// var future = 3;
+// var calcul = annee + future;
+// console.log(calcul);
+// document.write(calcul + "&nbsp &nbsp" + calcul + "<br>");
 
-var nb1, nb2, resultat;
-nb1 = 10;
-nb2 = 5;
+// var nb1, nb2, resultat;
+// nb1 = 10;
+// nb2 = 5;
 
-resultat = nb1 + nb2;
-console.log(resultat);
-document.write(resultat + "<br>");
+// resultat = nb1 + nb2;
+// console.log(resultat);
+// document.write(resultat + "<br>");
 
-var resultatSoustraction = nb1 - nb2;
-console.log(resultatSoustraction);
-document.write(resultatSoustraction + "<br>");
+// var resultatSoustraction = nb1 - nb2;
+// console.log(resultatSoustraction);
+// document.write(resultatSoustraction + "<br>");
 
-var resultatMultiplication = nb1 * nb2;
-console.log(resultatMultiplication);
-document.write(resultatMultiplication + "<br>");
+// var resultatMultiplication = nb1 * nb2;
+// console.log(resultatMultiplication);
+// document.write(resultatMultiplication + "<br>");
 
-var resultatDivision = nb1 / nb2;
-console.log(resultatDivision);
-document.write(resultatDivision + "<br>");
+// var resultatDivision = nb1 / nb2;
+// console.log(resultatDivision);
+// document.write(resultatDivision + "<br>");
 
-var resultatModulo= nb1 % nb2;
-console.log(resultatModulo);
-document.write(resultatModulo + "<br>");
+// var resultatModulo= nb1 % nb2;
+// console.log(resultatModulo);
+// document.write(resultatModulo + "<br>");
+
+var motDePasse = prompt ("Taper votre mot de passe")
+
+if (motDePasse < 50) {
+    document.write("Acces Refusée")
+} else {
+    document.write("Acces autorisée")
+}
